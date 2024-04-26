343c1378f9388273026e31d3db0ab8ebacc68c55
Tralion <68718850+trevorbollinger@users.noreply.github.com>
2024-04-26 18:35:22 -0500

    as

---
+++ JavaScript/main.js
@@ -0,0 +1,12 @@
+var allowRepeats = false;
+var stinkyCheck = false;
+var repeatCB = document.getElementById("uniqueCheck");
+var stinkyCB = document.getElementById("stinkyCheck");
+//CDS CARDS cardsu
+
+
+var ul = document.getElementById('myUL');
+ul.innerHTML = "";
+ul.innerHTML += listItems;
+
+updaetDisplay();
